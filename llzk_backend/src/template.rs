e2396ce73f685cadb9dbb12bcc25ab140034e971
Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
2026-01-13 08:19:23 -0600

    Replace FunctionContext::Drop with a finalize method (#309)

--- llzk_backend/src/template.rs
+++ llzk_backend/src/template.rs
@@ -156,7 +156,7 @@ impl<'ctx, 'str, 'func, 'blk, 'val> TemplateContext<'ctx, 'str, 'func, 'blk, 'va
     /// declarations to the declaring component.
     pub fn finalize(self, codegen: &LlzkCodegen<'_, 'ctx, impl ProgramLike>) -> Result<()> {
         let subcmps = self.subcmps;
-        self.and_then(
+        self.and_then::<_, _, GenResultUnit>(
             |fc, _| {
                 // Write the subcomponent declarations to self.
                 let self_value = fc.func.self_value_of_compute()?;
@@ -172,7 +172,8 @@ impl<'ctx, 'str, 'func, 'blk, 'val> TemplateContext<'ctx, 'str, 'func, 'blk, 'va
                 Ok(())
             },
             |_, _| Ok(()),
-        )
+        )?
+        .and_then_same(|fc, _| fc.finalize(codegen))
     }
 }
 
@@ -277,6 +278,10 @@ type GenResultSingleVal<'ctx, 'str, 'func, 'blk, 'val, 'r> =
 /// Alias for [GenResult] containing a list of SSA Value results.
 type GenResultMultiVal<'ctx, 'str, 'func, 'blk, 'val, 'r> =
     GenResult<'ctx, 'str, 'func, 'blk, 'val, 'r, Vec<Value<'ctx, 'val>>>;
+
+/// Alias for [GenResult] containing the unit type (i.e. nothing).
+type GenResultUnit<'ctx, 'str, 'func, 'blk, 'val, 'r> =
+    GenResult<'ctx, 'str, 'func, 'blk, 'val, 'r, ()>;
 
 /// This trait abstracts over the output type of [Chainable::and_then] to allow a single
 /// implementation of that function to produce different result types depending on the callback
@@ -1149,7 +1154,7 @@ where
                 // witness generator. Since LLZK currently has no such hint,
                 // we simply generate the underlying expression.
                 rhe.gen_llzk_in_template(codegen, template)
-            },
+            }
             Expression::Call { meta, id, args, .. }
                 if meta.get_type_knowledge().is_component()
                     && codegen.program.contains_template(id) =>
